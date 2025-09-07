9a4157a1867a56a167e3feb28e942b458879d58d
Beatriz Santos <beatrizodsk@gmail.com>
2025-09-07 15:29:15 -0300

    ✨ feat: add MenuItem model, migration and establish menu relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_07_180638) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_07_180717) do
+  create_table "menu_items", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.integer "menu_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["menu_id"], name: "index_menu_items_on_menu_id"
+  end
+
   create_table "menus", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "menu_items", "menus"
 end
