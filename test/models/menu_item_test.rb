9add54fd6a7251c5f2ac700288ea268505437b37
Beatriz Santos <beatrizodsk@gmail.com>
2025-09-08 23:10:15 -0300

    ✨ feat: implement RestaurantsController and update MenusController for restaurant association

--- test/models/menu_item_test.rb
+++ test/models/menu_item_test.rb
@@ -91,4 +91,32 @@ class MenuItemTest < ActiveSupport::TestCase
     assert_raises(ActiveRecord::RecordNotFound) { MenuItem.find(item_id) }
     assert_equal 0, menu.menu_items.count
   end
+
+  test 'name should be unique' do
+    menu_item1 = create(:menu_item, name: 'Pizza Margherita')
+    menu_item2 = build(:menu_item, name: 'Pizza Margherita')
+
+    assert_not menu_item2.valid?
+    assert_includes menu_item2.errors[:name], 'has already been taken'
+  end
+
+  test 'name uniqueness should be case sensitive' do
+    menu_item1 = create(:menu_item, name: 'Pizza Margherita')
+    menu_item2 = build(:menu_item, name: 'pizza margherita')
+
+    assert menu_item2.valid?
+  end
+
+  test 'should allow same menu item name across different restaurants' do
+    restaurant1 = create(:restaurant, name: 'Restaurant 1')
+    restaurant2 = create(:restaurant, name: 'Restaurant 2')
+    menu1 = create(:menu, restaurant: restaurant1, name: 'Menu 1')
+    menu2 = create(:menu, restaurant: restaurant2, name: 'Menu 2')
+
+    menu_item1 = create(:menu_item, name: 'Pizza Margherita', menu: menu1)
+    menu_item2 = build(:menu_item, name: 'Pizza Margherita', menu: menu2)
+
+    assert_not menu_item2.valid?
+    assert_includes menu_item2.errors[:name], 'has already been taken'
+  end
 end
