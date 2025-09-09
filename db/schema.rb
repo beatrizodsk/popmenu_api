4f481b1eb8c3d602fc15834c819e69ca77684059
Beatriz Santos <beatrizodsk@gmail.com>
2025-09-08 22:33:40 -0300

    ✨ feat: add Restaurant model and establish relationship with Menu

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_07_180717) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_09_011730) do
   create_table "menu_items", force: :cascade do |t|
     t.string "name"
     t.decimal "price"
@@ -24,7 +24,16 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_07_180717) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "restaurant_id", null: false
+    t.index ["restaurant_id"], name: "index_menus_on_restaurant_id"
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "menu_items", "menus"
+  add_foreign_key "menus", "restaurants"
 end
